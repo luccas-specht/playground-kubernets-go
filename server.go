eabf25c901c5b683349345a212ad1d48afff5429
Luccas Specht <luccas.specht.dev@gmail.com>
2025-10-17 07:59:31 -0300

    Implement health check and secret endpoints
    Added health check, secret, and config map endpoints with environment variable handling.

--- server.go
+++ server.go
@@ -1,14 +1,52 @@
 package main
 
 import (
-	"net/http"
+	"net/http"	
+	"os"
+	"fmt"
+	"log"
+	"io/ioutil"
+	"time"
 )
 
+var startedAt = time.Now()
+
 func main() {
+	http.HandleFunc("/healthz", Healthz)
+	http.HandleFunc("/secret", Secret)
+	http.HandleFunc("/configmap", ConfigMap)
 	http.HandleFunc("/", Hello)
 	http.ListenAndServe(":8000", nil)
 }
 
 func Hello(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("<h1>bah ne meu, World!</h1>"))
+	name := os.Getenv("NAME")
+	age := os.Getenv("AGE")
+	fmt.Fprintf(w, "Hello, %s! You are %s years old.", name, age)
+}
+
+func ConfigMap(w http.ResponseWriter, r *http.Request) {
+	  data, err := ioutil.ReadFile("opa/opa.txt")
+		if err != nil {
+			log.Fatalf("Error reading file: ", err)
+		}
+		fmt.Fprintf(w, "opass: %s", string(data))
+}
+
+func Secret(w http.ResponseWriter, r *http.Request) {
+	user := os.Getenv("USER")
+	password := os.Getenv("PASSWORD")
+	fmt.Fprintf(w, "Hello, %s! Your password is %s.", user, password)
+}
+
+func Healthz(w http.ResponseWriter, r *http.Request) {
+  duration := time.Since(startedAt)
+ 
+  if duration.Seconds() < 10 {
+    w.WriteHeader(500)
+    w.Write([]byte(fmt.Sprintf("Duration: %v", duration.Seconds())))
+  } else {
+    w.WriteHeader(200)
+    w.Write([]byte("ok"))
+  }
 }
